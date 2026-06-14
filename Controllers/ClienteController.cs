5bc70dea36d99b2a90a5073afb6d5fa8ecac10ca
GabrielAsserman <gabriel.asserman@gmail.com>
2026-06-14 20:29:25 -0300

    Add files via upload

--- Controllers/ClienteController.cs
+++ Controllers/ClienteController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using LojaApi.Data;
@@ -7,6 +8,7 @@ namespace LojaApi.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
+    [Authorize]
     public class ClienteController : ControllerBase
     {
         private readonly AppDbContext _context;
